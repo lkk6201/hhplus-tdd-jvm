9f32365eab2c4c0657befe7a42bc2766692fd1b5
lkk6201 <lkk6201@gmail.com>
2024-09-27 02:43:51 +0900

    - PointConfig 추가 - 설정파일에 포인트 최대 잔고 옵션 추가 - PointService 수정  => 포인트 충전 시 충전 포인트, 최대잔고 유효성 검사 추가  => 포인트 사용 시 히스토리 기록하도록 수정

--- src/main/java/io/hhplus/tdd/service/point/PointService.java
+++ src/main/java/io/hhplus/tdd/service/point/PointService.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.service.point;
 
+import io.hhplus.tdd.config.PointConfig;
 import io.hhplus.tdd.domain.point.PointHistory;
 import io.hhplus.tdd.domain.point.TransactionType;
 import io.hhplus.tdd.domain.point.UserPoint;
@@ -15,6 +16,7 @@ import java.util.List;
 public class PointService {
 
     private final PointRepository pointRepository;
+    private final PointConfig pointConfig;
 
     /**
      * 특정 유저의 포인트 조회
@@ -41,8 +43,17 @@ public class PointService {
      * @return
      */
     public UserPoint chargeUserPoint(long id, long amount) {
+        if (amount <= 0) {
+            throw new PointException("충전할 포인트는 0보다 큰 값을 가져야 합니다.");
+        }
+
         // 대상 UserPoint 조회
         UserPoint targetUserPoint = pointRepository.selectPointById(id);
+
+        // 최대 잔고 유효성 검사
+        if (amount + targetUserPoint.point() > pointConfig.getMaxBalance()) {
+            throw new PointException("충전 후 포인트 잔고는 " + pointConfig.getMaxBalance() + "포인트를 초과할 수 없습니다.");
+        }
 
         // 포인트 충전
         UserPoint updatedUserPoint = pointRepository.insertOrUpdate(id, targetUserPoint.point() + amount);
@@ -72,6 +83,11 @@ public class PointService {
             throw new PointException("잔여 포인트가 부족합니다.", currentPoint);
         }
 
-        return pointRepository.insertOrUpdate(id, currentPoint - amount);
+        // 포인트 사용
+        UserPoint updatedUserPoint = pointRepository.insertOrUpdate(id, currentPoint - amount);
+        // 포인트 사용 히스토리 기록
+        pointRepository.insertPointHistory(id, amount, TransactionType.USE, updatedUserPoint.updateMillis());
+
+        return updatedUserPoint;
     }
 }
